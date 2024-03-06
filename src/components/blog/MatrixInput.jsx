89fec9c5661a2930ebc1e443c6971fef63ec0b97
jeremy-london <jeremy.c.london@gmail.com>
2024-03-06 15:20:56 -0700

    fix: updates input handler for mobile

--- src/components/blog/MatrixInput.jsx
+++ src/components/blog/MatrixInput.jsx
@@ -4,9 +4,26 @@ const MatrixInput = ({ columns, idPrefix, defaultValues, onMatrixChange }) => {
   const [values, setValues] = useState(defaultValues);
 
   const handleInputChange = (row, column, value) => {
+    let newValue = value === '' ? '' : Number(value);
+    newValue = isNaN(newValue) ? '' : newValue;
+    updateValue(row, column, newValue);
+  };
+
+  const handleKeyDown = (row, column, event) => {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+      // Prevent the default input behavior
+      event.preventDefault();
+      const currentValue = values[row][column] || 0;
+      const increment = event.key === 'ArrowUp' ? 1 : -1;
+      const newValue = currentValue + increment;
+      updateValue(row, column, newValue);
+    }
+  };
+
+  const updateValue = (row, column, newValue) => {
     const newValues = [...values];
     newValues[row] = [...newValues[row]];
-    newValues[row][column] = Number(value);
+    newValues[row][column] = newValue;
     setValues(newValues);
     onMatrixChange(newValues);
   };
@@ -23,10 +40,13 @@ const MatrixInput = ({ columns, idPrefix, defaultValues, onMatrixChange }) => {
         {rowValues.map((value, columnIndex) => (
           <input
             key={`${idPrefix}-${rowIndex}-${columnIndex}`}
-            type="number"
+            type="text" // Use "text" for more control
+            inputMode="numeric" // Ensures numeric keyboard on mobile devices
+            pattern="[0-9]*" // Allows only numbers, but doesn't strictly enforce it
             className="text-center rounded-md border border-gray-200"
-            value={value}
+            value={value.toString()} // Convert value to string to handle empty and '0' values
             onChange={(e) => handleInputChange(rowIndex, columnIndex, e.target.value)}
+            onKeyDown={(e) => handleKeyDown(rowIndex, columnIndex, e)}
           />
         ))}
         </Fragment>
